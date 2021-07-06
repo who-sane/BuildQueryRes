829ca48994f2cf424d9be8c758c129121ea4c444
HUSSEIN HUSSEIN <103615588@student.swin.edu.au>
2021-07-06 10:21:52 +1000

    Task 1 Complete

--- Program.sql
+++ Program.sql
@@ -1,12 +1,19 @@
-﻿using System;
+﻿-- Hussein Hussein 
+-- 103615588
 
-namespace Resit
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+-- Task 1 
+
+Tour(TourName, Descriotion)
+PRIMARY KEY(TourName)
+
+Event(TourName, EventYear, EventMonth, EventDay, Fee)
+PRIMARY KEY(TourName, EventYear, EventMonth, EventDay)
+FOREIGN KEY(TourName) REFERENCES Tour
+
+Client(ClientID, Surname, GivenName, Gender)
+PRIMARY KEY(ClientID)
+
+Booking(TourName, EventYear, EventMonth, EventDay, ClientID, DateBooked, Payment)
+PRIMARY KEY(EventYear, EventMonth, EventDay, ClientID)
+FOREIGN KEY(TourName, EventYear, EventMonth, EventDay) REFERENCES Event
+FOREIGN KEY(ClientID) REFERENCES Client
